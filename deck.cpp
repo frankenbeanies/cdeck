99e45999413f526b557b36eb58ca68dddbd24256
Samuel Bostick <me@samuelbostick.com>
2016-10-06 22:45:58 -0400

    Added deck stack methods

---
+++ deck.cpp
@@ -0,0 +1,19 @@
+#include "deck.h"
+
+cdeck::Deck::Deck(){
+ 
+}
+
+void cdeck::Deck::add(cdeck::Card c){
+	_deck.push_back(c);
+}
+
+void cdeck::Deck::burn(cdeck::Card c){
+	_deck.insert(_deck.begin(), c);
+}
+
+cdeck::Card cdeck::Deck::draw(){
+	cdeck::Card c = _deck.back();
+	_deck.pop_back();
+	return c;
+}
---
+++ deck.h
@@ -0,0 +1,19 @@
+#ifndef DECK_H
+#define DECK_H
+
+#include "card.h"
+#include <vector>
+
+namespace cdeck{
+	class Deck{
+		private:
+			std::vector<cdeck::Card> _deck;
+		public:
+			Deck();
+			void add(cdeck::Card c);
+			void burn(cdeck::Card c);
+			cdeck::Card draw();
+	};
+};
+
+#endif
